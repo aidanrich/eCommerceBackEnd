6a5c16d9911421313e5018e26bc14699d9cf5cc2
aorich <rich.aidan@gmail.com>
2021-10-14 09:39:57 -0400

    added models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,15 +6,28 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id).then((categoryData) => {
+    res.json(categoryData);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then((newCategory) => {
+    res.json(newCategory);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
@@ -23,6 +36,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCat) => {
+      res.json(deletedCat);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
